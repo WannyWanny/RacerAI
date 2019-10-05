aaae4354dc7cf3ae751ec9845789345a338ac924
WannyWanny <lejent3@naver.com>
2019-10-05 22:02:16 +0900

    onduty

---
+++ Assets/UnityTechnologies/Micro-Games-Karting/KartTemplate/Scripts/RacerAcademy.cs
@@ -0,0 +1,27 @@
+﻿using MLAgents;
+using UnityEngine;
+
+public class RacerScript : Academy
+{
+    public Material KartMaterial;
+
+    public float kartRunSpeed;
+
+    public float kartReward;
+    public float kartPunish;
+
+    public override void InitializeAcademy()
+    {
+    }
+   
+
+    public override void AcademyReset()
+    {
+       
+    }
+
+    public override void AcademyStep()
+    {
+        
+    }
+}
